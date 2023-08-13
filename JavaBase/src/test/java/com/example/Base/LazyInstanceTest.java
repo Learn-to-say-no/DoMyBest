f505170e39b5f250995a541cf8f2210940e981d7
Learn-to-say-no <2734509332@qq.com>
2023-08-13 22:27:00 +0800

    init

---
+++ JavaBase/src/test/java/com/example/Base/LazyInstanceTest.java
@@ -0,0 +1,18 @@
+package com.example.Base;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * @description: some desc
+ * @author: muqingfeng
+ * @date: 2023/8/13 22:10
+ */
+public class LazyInstanceTest {
+
+    @Test
+    public void getInstance() {
+
+    }
+}
